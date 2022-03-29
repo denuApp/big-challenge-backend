c06ecc4ce579f467422d6a8c93689e38f01c5a5e
denuApp <denise@lightit.io>
2022-03-29 11:30:36 -0300

    fixes

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRegistrationRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
 
 class RegisterUserController extends Controller
 {
-
     public function __invoke(UserRegistrationRequest $request): JsonResponse
     {
         User::create($request->toArray());
