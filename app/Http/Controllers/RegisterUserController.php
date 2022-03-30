0513c952f43dba42f40f24259c69d2f197897f17
denuApp <denise@lightit.io>
2022-03-30 17:41:47 -0300

    create user resource

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -3,21 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegistrationRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterUserController extends Controller
 {
-    public function __invoke(UserRegistrationRequest $request): JsonResponse
+    public function __invoke(UserRegistrationRequest $request): UserResource
     {
         $request['password'] = Hash::make($request['password']);
 
-        User::create($request->validated());
+        $user = User::create($request->validated());
 
-        return response()->json([
-            'status' => 200,
-            'message' => 'New user added successfully!',
-        ]);
+        return new UserResource($user);
     }
 }
