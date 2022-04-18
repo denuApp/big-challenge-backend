0a63c78630814f4bd24c7b3e3403d91fc28fc85d
denuApp <denise@lightit.io>
2022-04-18 16:18:22 -0300

    test edit submission fails for invalid cases

--- tests/Feature/Submit/EditSubmissionsTest.php
+++ tests/Feature/Submit/EditSubmissionsTest.php
@@ -46,4 +46,69 @@ class EditSubmissionsTest extends TestCase
                 'symptoms' => $submission->symptoms,
             ]);
     }
+
+    public function test_submission_cant_be_edited_by_doctor()
+    {
+        $this->seed(UserPermissionsSeeder::class);
+
+        $patient = User::factory()->create();
+        $patient->assignRole('patient');
+        $submission = Submission::factory()->create(['patient_id' => $patient->id]);
+
+        $doctor = User::factory()->create();
+        $doctor->assignRole('doctor');
+
+        $symptoms = 'i have fever and cough';
+
+        Sanctum::actingAs($doctor);
+
+        $this
+            ->patchJson(
+                'api/edit-submission/'.$submission->id,
+                [
+                    'patient_id' => $patient->id,
+                    'symptoms' => $symptoms,
+                ]
+            )
+            ->assertForbidden();
+    }
+
+    /**
+     * @dataProvider invalidSubmissionProvider
+     */
+    public function test_invalid_submissions($edited)
+    {
+        $this->seed(UserPermissionsSeeder::class);
+
+        $patient = User::factory()->create(
+            ['id' => 1]
+        );
+        $patient->assignRole('patient');
+
+        Sanctum::actingAs($patient);
+
+        $submission = Submission::factory()->create(['patient_id' => $patient->id]);
+
+        $this
+            ->patchJson(
+                'api/edit-submission/'.$submission->id,
+                [
+                    $edited,
+                ]
+            )
+            ->assertUnprocessable();
+    }
+
+    public function invalidSubmissionProvider(): array
+    {
+        return [
+            ['incorrect patient id' => [
+                'patient_id' => 2,
+                'symptoms' => 'my arm hurt really bad',
+            ]],
+            ['empty symptoms' => [
+                'patient_id' => 1,
+            ]],
+        ];
+    }
 }
