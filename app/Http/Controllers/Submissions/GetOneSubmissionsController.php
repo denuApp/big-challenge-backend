1a0e1f1a4de9eb114492861e3866b8178f13e02b
denuApp <denise@lightit.io>
2022-04-19 14:21:03 -0300

    return a subission resource

--- app/Http/Controllers/Submissions/GetOneSubmissionsController.php
+++ app/Http/Controllers/Submissions/GetOneSubmissionsController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers\Submissions;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SubmissionResource;
 use App\Models\PatientInformation;
 use App\Models\Submission;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class GetOneSubmissionsController extends Controller
 {
-    public function __invoke(Submission $submission): JsonResponse
+    public function __invoke(Submission $submission): SubmissionResource
     {
         $info = PatientInformation::latest()->where('patient_id', Auth::user()->id)->get();
 
-        return response()->json([
-            'submissions' => $submission,
-            'info' => $info,
-        ]);
+        return new SubmissionResource($submission);
     }
 }
