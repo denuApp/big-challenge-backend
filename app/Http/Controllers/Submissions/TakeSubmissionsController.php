7a8427a73206368a6b5388886fec5f928c6dcd77
denuApp <denise@lightit.io>
2022-04-19 18:41:37 -0300

    test created

---
+++ app/Http/Controllers/Submissions/TakeSubmissionsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Submissions;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SubmissionResource;
+use App\Models\Submission;
+
+class TakeSubmissionsController extends Controller
+{
+    public function __invoke()
+    {
+//        $submission = Submission::create($request->validated());
+//
+//        return new SubmissionResource($submission);
+    }
+}
---
+++ tests/Feature/Diagnose/TakeSubmissionTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Diagnose;
+
+use App\Models\Submission;
+use App\Models\User;
+use Database\Seeders\UserPermissionsSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class TakeSubmissionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_doctor_can_take_patient_submission()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->seed(UserPermissionsSeeder::class);
+
+        $patient = User::factory()->create();
+        $patient->assignRole('patient');
+
+        $submission = Submission::factory()->create(['patient_id' => $patient->id]);
+
+        $doctor = User::factory()->create();
+        $doctor->assignRole('doctor');
+
+        Sanctum::actingAs($doctor);
+
+        $this
+            ->patch('api/take-submission/'.$submission->id, [
+                'doctor_id' => $doctor->id,
+            ])
+            ->assertSuccessful();
+
+        $this
+            ->assertDatabaseHas('submissions', [
+                'doctor_id' => $doctor->id,
+                'status' => 'in_progress',
+            ]);
+    }
+}
