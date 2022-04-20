5136934ece4c37a8d37426c111b0125077202630
denuApp <denise@lightit.io>
2022-04-20 13:36:40 -0300

    fixes

--- app/Http/Resources/PatientInformationResource.php
+++ app/Http/Resources/PatientInformationResource.php
@@ -19,7 +19,8 @@ class PatientInformationResource extends JsonResource
     public function toArray($request)
     {
         return[
-            'user_id' => $this->patient_id,
+            'id' => $this->id,
+            'patient_id' => $this->patient_id,
             'id_number' => $this->id_number,
             'gender' => $this->gender,
             'birth_date' =>$this->birth_date,
